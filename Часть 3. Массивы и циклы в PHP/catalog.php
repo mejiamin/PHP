7c85648a1ebd0748bf03227e0a4bdaee5847f656
Meji Amin <mokhmadmishiev@gmail.com>
2025-04-05 06:57:02 +0300

    Learned Part 3 of 5: Arrays and Loops in PHP

---
+++ Часть 3. Массивы и циклы в PHP/catalog.php
@@ -0,0 +1,13 @@
+<!-- catalog.php -->
+
+<?php
+require 'components/header.php';
+require 'products_db.php';
+
+$items = get_products();
+$filters = get_filters();
+$type = $_GET['product_type'];
+
+require 'components/products_list.php';
+require 'components/footer.php';
+?>
---
+++ Часть 3. Массивы и циклы в PHP/index.php
@@ -0,0 +1,11 @@
+<!-- index.php -->
+
+<?php
+require 'products_db.php';
+require 'components/header.php';
+
+$brands = ['cubic', 'dg'];
+
+require 'components/company_info.php';
+require 'components/footer.php';
+?>
